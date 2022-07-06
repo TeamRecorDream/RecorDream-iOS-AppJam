db83433f32fb3cfa5049308e05939aef09c97184
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-07-06 16:01:27 +0900

    [Set] 프로젝트 파일 추가 및 초기 세팅 (#1)

---
+++ RecorDream-iOS/RecorDream-iOS/Source/Application/AppDelegate.swift
@@ -0,0 +1,42 @@
+//
+//  AppDelegate.swift
+//  RecorDream-iOS
+//
+//  Created by 정은희 on 2022/07/06.
+//
+
+import UIKit
+
+import IQKeyboardManagerSwift
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        return true
+    }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.portrait
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ RecorDream-iOS/RecorDream-iOS/Source/Common/Constants/Constant.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  RecorDream-iOS
+//
+//  Created by 정은희 on 2022/07/06.
+//
+
+import UIKit
+
+enum Constant { }
