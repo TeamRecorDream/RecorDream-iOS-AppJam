8b727c615fd152f801a369c94c655c06e50bd87f
EunHee-Jeong <18102007@seoultech.ac.kr>
2022-07-11 14:11:33 +0900

    [Add] Unit Test용 세팅 파일 추가 (#5)

---
+++ RecorDream-iOS/RecorDreamTests/RecorDreamTests.swift
@@ -0,0 +1,25 @@
+//
+//  RecorDreamTests.swift
+//  RecorDreamTests
+//
+//  Created by 정은희 on 2022/07/11.
+//
+
+import XCTest
+@testable import RecorDream_iOS
+
+class RecorDreamTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // 테스트코드 시작 전 호출.
+        // 값을 세팅하는 부분
+    }
+
+    override func tearDownWithError() throws {
+        // 모든 테스트코드가 실행된 후 호출.
+        // 세팅된 값들을 해제하는 부분
+        
+    }
+
+    // test(), testPerformance() 함수를 만들어 테스트와 성능 측정을 합니다.
+}
