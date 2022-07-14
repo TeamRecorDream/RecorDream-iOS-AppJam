ae38518e21a42b2f8c1929a875b4ac1a26355006
LimYunHwi <88yhtserof@naver.com>
2022-07-14 18:36:09 +0900

    [Style]  마이페이지 화면 UI 구성 (#12)
    + 둥근 UIView 제외

---
+++ RecorDream-iOS/RecorDream-iOS/Source/Presentation/Play/ViewController/MyPageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MyPageViewController.swift
+//  RecorDream-iOS
+//
+//  Created by 임윤휘 on 2022/07/14.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
