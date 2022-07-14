8bf402f32c03b7fefdbcf63346c2e07afc646ddf
LimYunHwi <88yhtserof@naver.com>
2022-07-14 22:17:57 +0900

    [Style] 마이페이지 푸시알림 토글 커스텀 및 UI 완료 (#12)

--- RecorDream-iOS/RecorDream-iOS/Source/Presentation/Play/CustomSwitchButton.swift
+++ RecorDream-iOS/RecorDream-iOS/Source/Presentation/Play/CustomSwitchButton.swift
@@ -8,26 +8,30 @@
 import UIKit
 
 class CustomSwitchButton: UIButton {
+    //MARK: - UIView
+    
     private var barView: UIView?
     private var circleView: UIView?
+    
+    //MARK: - Properties
+    
+    private let onTintColor = ColorType.subPurple01.color
+    private let offTintColor = ColorType.black01.color
+    private let animationDuration: TimeInterval = 0.3
+    private let circleSize: CGFloat = 14
     private var circleVerticalMargin: CGFloat = 3
     private var circleHorizontalMargin: CGFloat = 4
-    
-    let onTintColor = ColorType.subPurple01.color
-    let offTintColor = ColorType.black01.color
-    let animationDuration: TimeInterval = 0.3
-    let circleSize: CGFloat = 14
-    var circleCenter: CGFloat = 0
-    
-    var isOn: Bool = false {
+    private var circleCenter: CGFloat = 0
+    private var isOn: Bool = false {
         didSet {
             self.barView?.backgroundColor = isOn ? onTintColor : offTintColor
-            self.circleCenter = frame.width
-            self.isSelected = isOn
             self.circleCenter = isOn ? frame.width - (circleHorizontalMargin + (circleSize/2)) : circleHorizontalMargin + (circleSize/2)
-            //나는 지금 원의 중심의 x값을 할당하고 있다!!!!
         }
     }
+    var completion:(() -> Void) = {}
+    
+    
+    //MARK: - Initialize
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -41,47 +45,44 @@ class CustomSwitchButton: UIButton {
         self.configureButton(frame: frame)
     }
     
+    //MARK: - Configure
+    
     private func configureButton(frame: CGRect){
-        print("frame.height: \(frame.height)")
-        print("frame.width : \(frame.width)")
-        print("frame.width - circleHorizontalMargin \(frame.width - circleHorizontalMargin)")
+        barView = {
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
+            view.backgroundColor = ColorType.black01.color
+            view.layer.cornerRadius = frame.height / 2
+            view.layer.masksToBounds = true
+            return view
+        }()
         
-        barView = UIView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
-        
-        barView?.backgroundColor = ColorType.black01.color
-        barView?.layer.cornerRadius = frame.height / 2
-        barView?.layer.masksToBounds = true
-        
-        guard let barView = barView else {return}
-        self.addSubview(barView)
-        
-        circleView = UIView(frame: CGRect(x: circleHorizontalMargin + (circleSize/2), y: circleVerticalMargin, width: circleSize, height: circleSize))
-        circleView?.backgroundColor = ColorType.white01.color
-        circleView?.layer.cornerRadius = circleSize / 2
-        circleView?.layer.masksToBounds = true
+        circleView = {
+            let view = UIView(frame: CGRect(x: circleHorizontalMargin + (circleSize/2), y: circleVerticalMargin, width: circleSize, height: circleSize))
+            view.backgroundColor = ColorType.white01.color
+            view.layer.cornerRadius = circleSize / 2
+            view.layer.masksToBounds = true
+            
+            return view
+        }()
         
         guard let circleView = circleView else {return}
-        self.addSubview(circleView)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        isOn.toggle()
-        configureAnimation()
-        if isOn {
-            print("on")
-        }else {
-            print("off")
-        }
+        guard let barView = barView else {return}
+        self.addSubviews(barView, circleView)
     }
     
     private func configureAnimation(){
         UIView.animate(withDuration: animationDuration, animations: {[weak self] in
             guard let self = self else {return}
             self.circleView?.center.x = self.circleCenter
-        }, completion: { [weak self] _ in
-            //guard let circleView = self.circleView else {return}
-            //self.circleView?.center.x = self.circleCenter
         })
+    }
+    
+    //MARK: - Action
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        isOn.toggle()
+        configureAnimation()
+        self.completion()
     }
 }
