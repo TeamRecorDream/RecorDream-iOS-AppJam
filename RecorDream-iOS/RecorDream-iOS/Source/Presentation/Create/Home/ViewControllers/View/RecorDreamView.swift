d23d3f2d4c9152b51c9f89393152192d0abb6c37
SojinLee <513sojin@naver.com>
2022-07-14 18:53:16 +0900

    [Feat] carousel 기능 구현 (#7)

--- RecorDream-iOS/RecorDream-iOS/Source/Presentation/Create/Home/ViewControllers/View/RecorDreamView.swift
+++ RecorDream-iOS/RecorDream-iOS/Source/Presentation/Create/Home/ViewControllers/View/RecorDreamView.swift
@@ -95,43 +95,93 @@ extension RecorDreamView: UICollectionViewDataSource, UICollectionViewDelegateFl
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CardCollectionViewCell.identifier, for: indexPath) as? CardCollectionViewCell else { return UICollectionViewCell() }
-//        print(cell)
         return cell
     }
     
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        scrollView.setContentOffset(CGPoint(x: 204.0 * CGFloat(cellIndex) + 10 * CGFloat(cellIndex), y: scrollView.contentInset.top), animated: true)
+    }
+    
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        let cellWidth = TypeConst.itemSize.width + TypeConst.itemSpacing
-        let offset = targetContentOffset.pointee.x + TypeConst.insetX
-        print("endDragging: \(offset)")
-        let index: CGFloat = round(offset / cellWidth)
+        //영모
+//        let x = scrollView.contentOffset.x
+//        for i in 0..<5 {
+//            let a = CGFloat((i == 0 ? 0 : 55) + (i == 0 ? 0 : 204 / 2) + (i > 1 ? 204 * (i - 1) + 20 * (i - 1) : 0))
+//            let b = a + 264 / 2 + (i == 0 ? 0 : 20)
+//            if x > a && x < b {
+//                scrollView.setContentOffset(CGPoint(x: a, y: scrollView.contentInset.top), animated: true)
+//            }
+//        }
+  
+//        let endX = scrollView.contentOffset.x
+//        let afterX = 180 * ((endX - startX) / (scrollView.contentSize.width / 2))
+//
+//        print("[END] \(startX) : \(endX) : \(afterX)")
+//        scrollView.setContentOffset(CGPoint(x: afterX + scrollView.contentInset.left, y: scrollView.contentInset.top), animated: true)
+//
+        //영모 2트
+//        0
+//        204 * 1 + 20 / 2
+//        204 * 2 + 20
+//        204 * 3 + 20 + 20 / 2
+//        204 * 4 + 20 + 20
+        scrollView.setContentOffset(CGPoint(x: 204.0 * CGFloat(cellIndex) + 10 * CGFloat(cellIndex), y: scrollView.contentInset.top), animated: true)
+//        scrollView.setContentOffset(CGPoint(x: 204 * self.cellIndex + 10 * self.cellIndex , y: scrollView.contentInset.top), animated: true)
+        
+        //소진 1ㅌ
+//        let x = scrollView.contentOffset.x
+//        for i in 0..<5 {
+//            let a = CGFloat(i * (204 + 20))
+//            let b = CGFloat(55 + i * (204 + 20) + 264 + 55)
+//            var xPoint: CGFloat
+//            if x > a && x < b {
+//                if ( x - 55.0 + CGFloat(i) * (204.0 + 20.0) - 55.0) > (264 / 2 + 55.0 ) {
+//                    xPoint = (CGFloat(i) + 1) * (204.0 + 20.0)
+//                } else {
+//                    xPoint = CGFloat(i) * (204.0 + 20.0)
+//                }
+//                scrollView.setContentOffset(CGPoint(x: xPoint, y: scrollView.contentInset.top), animated: true)
+//            }
+//        }
+        
+        // index 구하기
 
-        targetContentOffset.pointee = CGPoint(x: index * cellWidth - scrollView.contentInset.left, y: scrollView.contentInset.top)
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let cellWidth = TypeConst.itemSize.width + TypeConst.itemSpacing
-        let offset = scrollView.contentOffset.x
-        print("didScroll: \(offset)")
-        let index: CGFloat = round(offset / cellWidth)
-        cellIndex = Int(round(index))
-        print("cellIndex: \(cellIndex)")
-        carouselCollectionView.performBatchUpdates(nil) //TODO: -
-        let roundedIndex = round(index)
-        let indexPath = IndexPath(item: Int(roundedIndex), section: 0)
-        
-        if let cell = carouselCollectionView.cellForItem(at:indexPath) as? CardCollectionViewCell {
-            animateZoomforCell(zoomCell: cell)
-            cell.backgroundImage.alpha = 1
-//            cell.updateCell()#imageLiteral(resourceName: "simulator_screenshot_145A8932-7CB6-4A68-8A57-8CED952A9694.png")
-        }
-    
-        if Int(roundedIndex) != previousIndex {
-            let preIndexPath = IndexPath(item: previousIndex, section: 0)
-            if let preCell = collectionView(carouselCollectionView, cellForItemAt: preIndexPath) as? CardCollectionViewCell {
-                animateZoomforCellremove(zoomCell: preCell)
+        let x = scrollView.contentOffset.x
+        for i in 0..<5 { // TODO: - 카드 갯수에 따라 5에 해당되는 걸 바꾸기
+            let a = CGFloat(55 + 204 * i + 20 * (i > 1 ? i - 1 : 0)) - 102 + 20
+            let b = CGFloat(a + 264 + (i == 0 ? 0 : 20)) + 20
+            print("[D] \(x) \(a) \(b)")
+            if x > a && x < b {
+                cellIndex = i
+                carouselCollectionView.performBatchUpdates(nil)
             }
-            previousIndex = indexPath.item
         }
+//        let cellWidth = TypeConst.itemSize.width + TypeConst.itemSpacing
+//        let offset = scrollView.contentOffset.x
+//        print("didScroll: \(offset)")
+//        let index: CGFloat = round(offset / cellWidth)
+//        cellIndex = Int(round(index))
+//        print("cellIndex: \(cellIndex)")
+//        carouselCollectionView.performBatchUpdates(nil) //TODO: -
+//        let roundedIndex = round(index)
+//        let indexPath = IndexPath(item: Int(roundedIndex), section: 0)
+//
+//        if let cell = carouselCollectionView.cellForItem(at:indexPath) as? CardCollectionViewCell {
+//            animateZoomforCell(zoomCell: cell)
+//            cell.backgroundImage.alpha = 1
+////            cell.updateCell()#imageLiteral(resourceName: "simulator_screenshot_145A8932-7CB6-4A68-8A57-8CED952A9694.png")
+//        }
+//
+//        if Int(roundedIndex) != previousIndex {
+//            let preIndexPath = IndexPath(item: previousIndex, section: 0)
+//            if let preCell = collectionView(carouselCollectionView, cellForItemAt: preIndexPath) as? CardCollectionViewCell {
+//                animateZoomforCellremove(zoomCell: preCell)
+//            }
+//            previousIndex = indexPath.item
+//        }
     
     }
     
