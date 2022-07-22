e4ca83ca446a5cb812dfcdae9603cbe509b4b588
LimYunHwi <88yhtserof@naver.com>
2022-07-23 01:00:15 +0900

    [Add] Constant 상세보기 카드이미지 확장 (#46)

--- RecorDream-iOS/RecorDream-iOS/Source/Common/Constants/RecorDreamColorType.swift
+++ RecorDream-iOS/RecorDream-iOS/Source/Common/Constants/RecorDreamColorType.swift
@@ -33,4 +33,17 @@ extension Constant {
             }
         }
     }
+    
+    enum DetailBackgroundColor {
+        static let detailBackgroundColorTitles = [ImageList.detailCardColorDark.name, ImageList.detailCardColorGreen.name, ImageList.detailCardColorBlue.name, ImageList.detailCardColorOrange.name, ImageList.detailCardColorPurple.name,  ImageList.detailCardColorPink.name, ImageList.detailCardColorRed.name]
+        
+        case IntType(Int)
+        
+        var title: String {
+            switch self {
+            case .IntType(let backgroundColorNum):
+                return DetailBackgroundColor.detailBackgroundColorTitles[ backgroundColorNum >= 0 && backgroundColorNum < DetailBackgroundColor.detailBackgroundColorTitles.count ? backgroundColorNum : 0]
+            }
+        }
+    }
 }
